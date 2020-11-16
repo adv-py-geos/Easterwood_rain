58d7d6e50bb4de92c0364b78ead3fa4003334ed5
askia97 <60488184+askia97@users.noreply.github.com>
2020-11-16 13:31:41 -0600

    Delete daily_average.py
    no need for this script.

--- daily_average.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-def mean_d(station):
-	''' Takes a pandas dataframe of the station, assuming it's been indexing by Datetime, then outputs its 	daily average'''
-
-	station_daily = station.resample('1d').mean()
-	
-	return station_daily
